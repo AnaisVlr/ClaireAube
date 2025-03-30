ac08ea8153d0ebfdc0a677e6150140600e3c8513
Anaïs Velcker <anais.velcker@gmail.com>
2025-03-30 18:50:01 +0200

    Suite page soins

--- src/pages/Care/CareRituals.tsx
+++ src/pages/Care/CareRituals.tsx
@@ -1,29 +1,38 @@
 import flowerHand from '../../assets/img/flower_hand.webp'
+import ritualPicture from '../../assets/img/soins-rituels/soins-rituels-1.webp'
+import carteSaison from '../../assets/img/soins-rituels/soins-rituels-4.webp'
+import yule from '../../assets/img/soins-rituels/yule-1.webp'
+import gardienTerre from '../../assets/img/soins-rituels/gardien-de-la-terre-1.webp'
+
 import './careRituals.css'
 
 export default function CareRituals() {
   return (
     <div className="care-rituals">
+
       <div className="banner">
-        <h2>Soins Rituels</h2>
-        <p>De la peau à l'âme</p>
+        <div className="banner-text">
+          <h2>Soins Rituels</h2>
+          <p>De la peau à l'âme</p>
+        </div>
       </div>
+
       <div className="introduction">
-        <img src={flowerHand} alt="Logo Claire Aube" loading="lazy"/>
+        <img src={ritualPicture} alt="" loading="lazy"/>
         <p>
-          Il est évident pour moi que le chemin de guérison passe par le corps... <br/>
-          Le fait de prendre soin, de chérir, de magnifier sa beauté... a toujours fait partie des routines des communautés de femmes. <br/>
-          Ainsi, je vous propose de renouer avec ces traditions, cette sagesse du toucher. <br/>
-          Plus que des soins, de vrais rituels transformateurs. <br/>
-          Des espaces vibrants et magiques pour vous retrouver, vous cocooner, vous ramener à votre essence primordiale. <br/>
+          Il est évident pour moi que le chemin de guérison passe par le corps... <br/><br/>
+          Le fait de prendre soin, de chérir, de magnifier sa beauté... a toujours fait partie des routines des communautés de femmes. <br/><br/>
+          Ainsi, je vous propose de renouer avec ces traditions, cette sagesse du toucher. <br/><br/>
+          Plus que des soins, de vrais rituels transformateurs. <br/><br/>
+          Des espaces vibrants et magiques pour vous retrouver, vous cocooner, vous ramener à votre essence primordiale. <br/><br/>
           Pour honorer votre incarnation humaine.
         </p>
       </div>
       <div className="introduction-suite">
         <p>
-          Chaque soin a été conçu avec la plus grande attention, avec une profondeur de réflexion intime. <br/>
-          Fruits de tous les enseignements et sagesses que j’ai récolté ces dernières années, et de mon propre cheminement personnel.<br/>
-          Je me sens aujourd’hui particulièrement honorée de mettre mon toucher sacré au service de vos corps et vos âmes.<br/>
+          Chaque soin a été conçu avec la plus grande attention, avec une profondeur de réflexion intime. <br/><br/>
+          Fruits de tous les enseignements et sagesses que j’ai récolté ces dernières années, et de mon propre cheminement personnel.<br/><br/>
+          Je me sens aujourd’hui particulièrement honorée de mettre mon toucher sacré au service de vos corps et vos âmes.
         </p>
       </div>
       <div className='rituals-for'>
@@ -45,6 +54,199 @@ export default function CareRituals() {
           Ils sont empreints de nombreux enseignements issus des médecines ancestrales, telle que la médecine chinoise, la médecine ayurvédique, 
           la médecine taoïste, la médecine amérindienne... <br/> Et, n’oublions pas, la richesse de notre médecine traditionnelle européenne !
         </p>
+
+      </div>
+      <div>
+        <h4>Selon le soin, vous pourrez vous voir proposer :</h4>
+        <ul>
+          <li>De la visualisation</li>
+          <li>Du massage</li>
+          <li>Des étirements doux</li>
+          <li>Des soins physico-énergétiques avec les onctions sacrées</li>
+          <li>Des serrages, bercements, bordages et enveloppement aux tissus</li>
+          <li>Des pratiques de bain et yonisteam</li>
+          <li>Des remèdes de grand-mère tels que les cataplasmes et bouillottes</li>
+          <li>Des pochons chauds</li>
+          <li>Des massages au kansa wand</li>
+          <li>Des tirages d’oracle</li>
+          <li>Des rituels psycho-magiques</li>
+          <li>Etc...</li>
+        </ul>
+      </div>
+      <div className="rituals-description">
+        <h3>Découvrez tous mes rituels de soin</h3>
+
+        <div className="rituals-line">
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel de la poitrine et du coeur</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel Matricia</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel Chrysalide</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+        </div>
+
+        <div className="ritual-category">
+          Les rituels pour les mamans
+        </div>
+
+        <div className="rituals-line">
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel des Matronae</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel du bain des Relevailles</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+        </div>
+
+        <div className="ritual-category">
+          Les rituels saisonniers
+        </div>
+
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Odit quam quis fugit quaerat illo ab, molestiae minus. 
+          Eligendi nisi cupiditate vel iusto omnis laudantium officia fugit expedita quod? Quo, itaque.
+        </div>
+
+        <div className="season-ritual">
+          <img src={carteSaison} alt="Image avec la roue des saisons Celtes" loading="lazy"/>
+          
+          <div>
+            <h3>En ce moment, découvrez le rituels de Yule :</h3>
+            <div className="ritual-item">
+              <img src={yule} alt="" loading="lazy"/>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+                sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+                iste soluta nisi! Nihil soluta labore sequi voluptas.
+              </p>
+              <button>En savoir +</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="ritual-category">
+          Les rituels sur mesure
+        </div>
+
+        <div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, dolor explicabo. Ipsa, 
+            modi repellendus nulla ratione iusto voluptatibus eum voluptate obcaecati nobis? Vitae,
+             temporibus perferendis minus laborum atque odit molestias.
+          </p>
+
+          <span>
+            Des soins signature, entièrement personnalisés, pensés pour vous et par vous
+          </span>
+
+          <div className="rituals-line">
+          <div className="ritual-item">
+            <img src={gardienTerre} alt="" loading="lazy"/>
+            <h6>Rituel des Gardiens de la Terre</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+
+          <div className="ritual-item">
+            <img src={flowerHand} alt="" loading="lazy"/>
+            <h6>Rituel Onctions Sacrées</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates, 
+              sint cupiditate culpa, vitae voluptas excepturi vel et beatae fugiat eos maiores 
+              iste soluta nisi! Nihil soluta labore sequi voluptas.
+            </p>
+            <button>En savoir +</button>
+          </div>
+        </div>
+        </div>
+      
+      </div>
+
+      <div className="explication-rituals">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic reiciendis placeat eos 
+          officiis eveniet? Quibusdam quidem provident, quasi cupiditate at nam, amet nemo 
+          ullam dolores temporibus suscipit vel corporis fuga?
+        </p>
+      </div>
+
+      <div className="space">
+        <h3>Mon espace de soin :</h3>
+        <div>
+          <div>
+            <img src={flowerHand} alt="Logo Claire Aube" loading="lazy"/>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic reiciendis placeat eos 
+              officiis eveniet? Quibusdam quidem provident, quasi cupiditate at nam, amet nemo 
+              ullam dolores temporibus suscipit vel corporis fuga?
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="rituals-contact">
+        <img src={flowerHand} alt="Logo Claire Aube" loading="lazy"/>
+        <div>
+          <p>
+            Vous vous sentez appelée à vivre un rituel avec moi ? <br/>
+            Vous ne savez pas exactement lequel choisir ?
+          </p>
+          <button>
+            Contactez-moi
+          </button>
+        </div>
+        <img src={flowerHand} alt="Logo Claire Aube" loading="lazy"/>
+      </div>
+
+      <div className="ritual-gift">
 
       </div>
 
