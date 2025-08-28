979822ec98e1305d56863becf64723b5a002ebac
Anaïs Velcker <anais.velcker@gmail.com>
2025-08-28 18:29:59 +0200

    Avancements Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,78 +7,82 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import { useTexts } from "../hooks/useTexts";
 
 export default function Footer() {
+  const t = useTexts();
+
   const navItems = [
-    { label: "À propos", href: "/about" },
-    { label: "CGS", href: "/CGS" },
-    { label: "Politique de confidentialité", href: "/politique-de-confidentialite" },
-    { label: "Médical disclaimer", href: "/medical-disclaimer" },
-    { label: "Mentions légales", href: "/mentions-legales" },
+    { label: t.aboutMe.aboutMe, href: "/about" },
+    { label: t.general.CGS, href: "/CGS" },
+    { label: t.general.privatePolicy, href: "/politique-de-confidentialite" },
+    { label: t.general.medicalDisclaimer, href: "/medical-disclaimer" },
+    { label: t.general.legalMention, href: "/mentions-legales" },
   ];
 
   return (
     <>
       <footer>
-        <Grid size={12} container spacing={2}>
-          <Grid size={12} className="flex-row align-center justify-between">
-            <div className="flex-column gap-1">
-              <div className="gap-50 flex-column">
-                <h2>Claire Aube</h2>
-                <p className="head-title">Thérapeute holistique en santé<br />Féminine et Ritualiste</p>
-              </div>
-              <nav className="flex-column gap-50">
-                {navItems.map((item) => (
-                  <div>
+        <Grid
+          size={12}
+          container
+          spacing={2}
+          flexDirection={"column"}
+          px={{ xs: 2, sm: 8 }}
+          py={{ xs: 2, sm: 4 }}
+        >
+
+          <Grid size={12} container spacing={2} flexDirection={{ xs: "column", sm: "row" }}>
+            {/* Pages */}
+            <Grid size={{ xs: 12, sm: 4 }} container spacing={2} flexDirection={"column"}>
+              <Grid>
+                <Typography variant="h4" fontWeight={700} className="typoTanPearl">{t.general.fullName}</Typography>
+                <Typography fontWeight={500}>{t.general.job}</Typography>
+              </Grid>
+              <nav>
+                <Grid size={12} container spacing={1} flexDirection={"column"}>
+                  {navItems.map((item) => (
                     <Link to={item.href}>
                       {item.label}
                     </Link>
-                  </div>
-                ))}
+                  ))}
+                </Grid>
               </nav>
-            </div>
-            <div className="flex-column gap-1">
-              <img src={logoTerracota} alt="Logo Claire Aube" className='logo' loading="lazy" />
-            </div>
-            <div className="flex-column gap-1">
-              <div className="instagram-title">
-                Retrouve moi sur Instagram
-                <FontAwesomeIcon icon={faInstagram} className='instagram-icon' />
-              </div>
-              <div className="flex-column gap-1 align-center">
-                <div className="flex-row gap-1 justify-center">
-                  <div className="instagram-feed-item">
-                    <img src="" alt="Instagram feed" />
-                  </div>
-                  <div className="instagram-feed-item">
-                    <img src="" alt="Instagram feed" />
-                  </div>
-                  <div className="instagram-feed-item">
-                    <img src="" alt="Instagram feed" />
-                  </div>
-                </div>
-              </div>
-              <div className="flex-column gap-1 align-center">
-                <Link to="/contact" className="btn btn-sm btn-primary">
-                  Me contacter
+            </Grid>
+            {/* Logo */}
+            <Grid size={{ xs: 12, sm: 4 }} textAlign={"center"}>
+              <img src={logoTerracota} alt="Logo Claire Aube" loading="lazy" width={"80%"} />
+            </Grid>
+            {/* Contact */}
+            <Grid size={{ xs: 12, sm: 4 }}>
+              <Typography fontWeight={700}>
+                {t.general.findMeOnInsta}
+                <FontAwesomeIcon icon={faInstagram} />
+              </Typography>
+              <Grid>
+                {/* <InstagramPost url="https://www.instagram.com/p/DN5MBPzjAHi/?utm_source=ig_web_copy_link" /> */}
+              </Grid>
+              <Grid>
+                <Link to="/contact">
+                  {t.general.contactMe}
                 </Link>
-                <div className="flex-row gap-2">
-                  <button className="btn-sm btn-outline-primary">
-                    <FontAwesomeIcon icon={faEnvelope} className='contact-icon' />
-                  </button>
-                  <button className="btn-sm btn-primary">
-                    <FontAwesomeIcon icon={faPhone} className='contact-icon' />
-                  </button>
-                </div>
-              </div>
-            </div>
+                <Grid>
+                  <Button variant={"contained"}>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </Button>
+                  <Button variant={"contained"}>
+                    <FontAwesomeIcon icon={faPhone} />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
-          <div className="flex-row align-center justify-between gap-2 pt-2">
-            <p>
+          <Grid >
+            <Typography textAlign={{ xs: "center", sm: "left" }}>
               © 2025 Claire Aube - Tous droits réservés - Made by Anaïs
-            </p>
-          </div>
+            </Typography>
+          </Grid>
         </Grid>
       </footer>
     </>
